5a4da7cab6a4d1e20225614c0e9b41a997d2c731
tmoiroud <thib.moiroud@gmail.fr>
2017-02-23 23:53:55 +0100

    Ajout basique de routes + template pug

--- src/app.js
+++ src/app.js
@@ -1,13 +1,45 @@
+// Import de modules
+import * as parser from 'body-parser'
+import * as express from 'express'
 import * as http from 'http'
+import { join } from 'path'
+import * as io from 'socket.io'
 
-let app = http.createServer((req, res) => {
-  let tab = [1, 2, 3, 4]
+// Coeur de l'application
+let app = express()
+let server = http.createServer(app)
+let io = io.listen(server)
 
-  tab.forEach(e => {
-    console.log(e, ...tab)
-  })
+// Configuration de l'application
+app.set('view engine', 'pug')
+app.use(parser.urlencoded())
+app.use(parser.json())
+app.use('/static', express.static(join(__dirname, '/static')))
 
-  res.end('Hey!')
+// Routes
+app.route('/')
+.get((req, res) => {
+  res.render('index', {title: 'Accueil'})
+})
+.post((req, res) => {
+})
+
+app.route('/play')
+.get((req, res) => {
+  res.render('play', {title: 'Questionnaire'})
+})
+.post((req, res) => {
+})
+
+app.route('/upload')
+.get((req, res) => {
+  res.render('upload', {title: 'Dépot de questionnaire'})
+})
+.post((req, res) => {
 })
 
 app.listen(3000)
+
+// Utilisation du socket
+io.sockets.on('connection', (socket) => {
+})
